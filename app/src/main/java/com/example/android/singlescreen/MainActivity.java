2e26dd58fa6a11967819fef42530164e2ac0ea84
sijanr <47872289+sijanr@users.noreply.github.com>
2019-03-12 15:00:14 -0700

    Add intents to phone, mail and location

--- app/src/main/java/com/example/android/singlescreen/MainActivity.java
+++ app/src/main/java/com/example/android/singlescreen/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.singlescreen;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,5 +19,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton mailButton = (ImageButton) findViewById(R.id.mail_image_view);
+        ImageButton phoneButton = (ImageButton) findViewById(R.id.phone_image_view);
+        ImageView locationButton = (ImageButton) findViewById(R.id.location_image_view);
+
+
+//       open an email app when the mail button is clicked
+        mailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] recipient = {getString(R.string.email)};
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, recipient);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+//       open a dialer when the phone button is clicked
+        phoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + getString(R.string.contact_number)));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+//       open a map when the location button is clicked
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:0,0?q=321+East+Chapman+Avenue%2CFullerton%2C+CA"));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 }
